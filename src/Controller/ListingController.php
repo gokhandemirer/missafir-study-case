b0ddc2dcb061520fff82c1689c7406e028590b82
gokhandemirer <gdmr34@gmail.com>
2023-02-19 12:45:03 +0300

    Use validation trait in our controller, add exception handling, fetch available listings via repository

--- src/Controller/ListingController.php
+++ src/Controller/ListingController.php
@@ -2,16 +2,51 @@
 
 namespace App\Controller;
 
+use App\Exception\InvalidDateParameterException;
+use App\Exception\SearchRequestMissingParameterException;
+use App\Repository\ListingRepository;
+use App\Trait\ValidatesSearchRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Exception\JsonException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ListingController extends AbstractController
 {
+    use ValidatesSearchRequest;
+
+    public function __construct(
+        protected ListingRepository $listingRepository
+    )
+    {}
+    
     #[Route('/listing/search', name: 'listing_search', methods: ['POST'])]
     public function search(Request $request): JsonResponse
     {
-        return $this->json(['message' => 'OK']);
+        try {
+            $requestAsArray = $request->toArray();
+            $this->validateRequest($requestAsArray);
+
+            $listings = $this->listingRepository->findAvailableListingsByDateRange(
+                $requestAsArray['startDate'],
+                $requestAsArray['endDate']
+            );
+
+            return $this->json(compact('listings'));
+        } catch (JsonException $e) {
+            return $this->json([
+                'message' => $e->getMessage()
+            ], $e->getCode());
+        } catch (SearchRequestMissingParameterException $e) {
+            return $this->json([
+                'message' => $e->getMessage(),
+                'field'   => $e->getField()
+            ], $e->getCode());
+        } catch (InvalidDateParameterException $e) {
+            return $this->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 }
